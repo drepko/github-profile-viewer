4cb8904afbbe27490ecba2ed45784926c9bedfc6
Danielle Repko <daniellerepko@Danielles-MacBook-Air.local>
2024-02-29 17:05:39 +0100

    fix home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div className='mt-8 items-center space-y-4'>
-      <div className='flex w-full justify-center text-3xl font-bold'>HOME</div>
+      <div className='flex w-full flex-col items-center justify-center text-3xl font-bold'>
+        GITHUB PROFILE VIEWER
+        <Link
+          href='/users'
+          className='my-4 rounded border bg-gray-100 px-3 py-1 text-sm text-gray-800'
+        >
+          Get started
+        </Link>
+      </div>
     </div>
   );
 }
